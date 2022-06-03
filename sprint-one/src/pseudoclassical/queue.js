251a9329e5989a26beffc61e91f7938c48d611b5
Peter McBride <mrpetermcbride@gmail.com>
2022-06-03 11:04:03 -0700

    Completed pseudoclassical Class for Queue and Stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.addedLast = 0;
+  this.firstValue = 0;
 };
+
+//first in / first out
+
+Queue.prototype.enqueue = function(value) {
+  // Add value to queue and increment addedLast
+  this.addedLast++;
+  this[this.addedLast] = value;
+
+};
+
+Queue.prototype.dequeue = function() {
+  // check if there are any values in queue
+  // delete the first value
+  if (this.addedLast - this.firstValue > 0) {
+    this.firstValue++;
+    var firstProp = this[this.firstValue];
+    delete this[this.firstValue];
+    return firstProp;
+  }
+
+};
+
+Queue.prototype.size = function() {
+  return this.addedLast - this.firstValue;
+
+};
+
+
+var queueMethods = new Queue();
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.counter = 0;
+};
+//last in / First Out
+Stack.prototype.push = function (value) {
+  this.counter++;
+  this[this.counter] = value;
+
+
 };
 
+Stack.prototype.pop = function () {
+  // check if there is anything in the stack
+  if (this.counter > 0) {
+    var lastProperty = this[this.counter];
+    delete this[this.counter];
+    this.counter--;
+    return lastProperty;
+  }
+
+};
+
+Stack.prototype.size = function () {
+  return this.counter;
+
+};
+
+var stackMethods = new Stack ();
 
