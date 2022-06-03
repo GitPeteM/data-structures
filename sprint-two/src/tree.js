e7289ec2ce5dbbd5867b2931a756c18e34a9ed84
BlandSchwad <schwaderer@protonmail.com>
2022-06-03 16:30:33 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +12,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  val = Tree(value);
+  // assign value as the next node. 
+  // add that node to the children array (tree)
+  // console.log(value);
+  this.children.push(val);
+  // console.log(this.children);
+
+  
 };
 
 treeMethods.contains = function(target) {
+  // check if target exists within family tree and return Boolean value
+  // while current child is a node
+  console.log(this.children);
+  
+  if (target === this.value) {
+    return true;
+  } else if (this.children.length > 0) { 
+    var targetFound = false;
+    for (var i = 0; i < this.children.length; i++) {
+      if (targetFound === false) {
+        targetFound = this.children[i].contains(target);
+      }
+    }
+    return targetFound;
+
+  }
+  return false;
+
 };
 
 
