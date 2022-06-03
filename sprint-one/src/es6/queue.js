f8063bbfcb8676e18469285acbed0ad0ca25d72b
Peter McBride <mrpetermcbride@gmail.com>
2022-06-03 11:29:17 -0700

    Completed es6 class implementation on Data Structures

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -3,6 +3,35 @@ class Queue {
   // but try not not reference your old code in writing the new style.
 
   constructor() {
+
+    this.addedLast = 0;
+    this.firstValue = 0;
   }
 
+  // first in / last out
+  enqueue(value) {
+    // Add a value to the queue
+    this.addedLast++;
+    this[this.addedLast] = value;
+  }
+
+  dequeue() {
+    // remove a value from the queue
+    if (this.addedLast - this.firstValue > 0) {
+      this.firstValue++;
+      var firstProp = this[this.firstValue];
+      delete this[this.firstValue];
+      return firstProp;
+    }
+
+  }
+
+  size() {
+    return this.addedLast - this.firstValue;
+
+  }
+
+
 }
+
+var queueMethods = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -3,6 +3,32 @@ class Stack {
   // but try not not reference your old code in writing the new style.
 
   constructor() {
+    this.counter = 0;
+  }
+
+  // first in / last out
+
+  push(value) {
+    // Add value to stack.
+    this.counter++;
+    this[this.counter] = value;
+  }
+
+  pop() {
+    // remove value from stack
+    if (this.counter > 0) {
+      var lastProperty = this[this.counter];
+      delete this[this.counter];
+      this.counter--;
+      return lastProperty;
+    }
+  }
+
+  size() {
+    return this.counter;
+
   }
 
 }
+
+var queueMethods = new Stack();
